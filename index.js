5b7d28742fcc76b95b8dfed5ee66ebee874c50aa
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2025-11-16 10:27:54 -0700

    small problem w/ function findAvgRate. Backing up what works

--- index.js
+++ index.js
@@ -30,8 +30,31 @@ const humanAssetList = () => {
   return { name, occupation, priceRange };
 };
 
+// Initialize a state variable to an array of `NUM_FREELANCERS` freelancer objects.
 const humanAssetArray = Array.from({ length: NUM_FREELANCERS }, humanAssetList);
 
 console.log(humanAssetArray);
 
-// Initialize a state variable to an array of `NUM_FREELANCERS` freelancer objects.
+//  function returns the average rate of all freelancers
+const findAvgRate = () => {
+  const getRates = humanAssetArray.map((getRate) => getRate.priceRange);
+  console.log(getRates);
+
+  getRates.forEach((rateValue) => {
+    rateValue.min = Number(rateValue.min);
+    rateValue.max = Number(rateValue.max);
+  });
+  console.log(getRates);
+
+  const medianRates = getRates.map(
+    (medianRate) => (medianRate.min + medianRate.max) / 2
+  );
+  const averageRates = medianRates/* .toFixed(2) */;
+  console.log(averageRates);
+};
+
+// map humanAssetArray to extract priceRange
+//  map priceRange to produce an array of median rates: (min + max)/2
+// return (median rates)/(NUM_ FREELANCERS)
+
+findAvgRate();
